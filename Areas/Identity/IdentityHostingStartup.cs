0e245e884d83212b514f603bffe2fa62125843f6
Julia Farganus <juliafarganus@gmail.com>
2024-01-23 14:20:07 +0100

    identity

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -15,6 +15,7 @@ namespace DotNetLab12.Areas.Identity
         public void Configure(IWebHostBuilder builder)
         {
             builder.ConfigureServices((context, services) => {
+               
             });
         }
     }
